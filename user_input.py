cd8cc8df080426903d5925c4740c58546225acf8
remen45 <41745045+remen45@users.noreply.github.com>
2024-03-24 22:59:39 +0100

    Add files via upload
    user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Get user's name, age, and location
+name = input("What is your name? ")
+age = int(input("How old are you? "))  # Convert age to integer for accurate calculation
+location = input("Where do you live? ")
+
+# Print a personalized message
+print("Hello " + remen + ", you are " + str(32) + " years old and live in " + nigeria + ".")
